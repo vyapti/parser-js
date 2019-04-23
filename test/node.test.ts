be200b03c08ef28abd5aae5acdfcd7da5472f3e4
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-04-22 22:16:14 -0500

    Update/Add Top Level Export Tests
    This commit adds unit tests for the new top level exports and updates the existing index.test.ts to match the new exports available.

---
+++ test/node.test.ts
@@ -0,0 +1,11 @@
+import { assert } from 'chai';
+import 'mocha';
+
+import topLevelNode from '../src/node';
+
+it('top level node export should expose correct elements', () => {
+  assert.isDefined(topLevelNode.ContentParser, 'Content Parser should be exposed');
+  assert.isDefined(topLevelNode.FileParser, 'File Parser should be exposed');
+  assert.isDefined(topLevelNode.ReportMode, 'Report Mode should be exposed');
+  assert.isDefined(topLevelNode.StreamParser, 'Stream Parser should be exposed');
+});
---
+++ test/web.test.ts
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+import 'mocha';
+
+import topLevelWeb from '../src/web';
+
+it('top level web export should expose correct elements', () => {
+  assert.isDefined(topLevelWeb.ContentParser, 'Content Parser should be exposed');
+  assert.isDefined(topLevelWeb.ReportMode, 'Report Mode should be exposed');
+  assert.isDefined(topLevelWeb.StreamParser, 'Stream Parser should be exposed');
+});
