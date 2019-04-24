5af4d62c5c3957a2d3a63548dbf9b6abb0fc1f7f
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-04-24 16:40:00 -0500

    Adjust top level export to consolidate stream parsers
    This commit updates the top level export to consolidate the variants of stream parser. A check is made at runtime to decide which implementation to export. This allows consumers to only reference "StreamParser" but get the correct variant they need automatically.
    
    Tests have been updated to reflect the changes.

--- test/index.test.ts
+++ test/index.test.ts
@@ -7,12 +7,5 @@ it('top level export should expose correct elements', () => {
   assert.isDefined(topLevel.ContentParser, 'Content Parser should be exposed');
   assert.isDefined(topLevel.FileParser, 'File Parser should be exposed');
   assert.isDefined(topLevel.ReportMode, 'Report Mode should be exposed');
-  assert.isDefined(
-    topLevel.NodeStreamParser,
-    'Node Stream Parser should be exposed',
-  );
-  assert.isDefined(
-    topLevel.BrowserStreamParser,
-    'Browser Stream Parser should be exposed',
-  );
+  assert.isDefined(topLevel.StreamParser, 'Stream Parser should be exposed');
 });
