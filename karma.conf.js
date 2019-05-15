9178aacbcd2006a41a074c17fe0daa1638a3bb51
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-05-15 03:20:59 -0500

    Remove jsdom from browser list
    This commit removes jsdom from the browser list. Having both jsdom and chrome headless is somewhat redundant since it adds another suite of tests to run in a similar environment.

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'ChromeHeadless', 'Firefox', 'jsdom'],
+    browsers: ['Chrome', 'ChromeHeadless', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
