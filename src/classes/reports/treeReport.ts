ea1d2dd3aa0842167ed7e2d5641800deafe4f828
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2019-05-17 02:08:20 -0500

    Add Doc Comments for Reports

--- src/classes/reports/treeReport.ts
+++ src/classes/reports/treeReport.ts
@@ -3,7 +3,19 @@ import Summary from '../summaries/summary';
 import Report from './report';
 import { NodeOrLeaf } from './treeReportNode';
 
+/**
+ * Tree Coverage Report
+ *
+ * An extension of {@link Report | Report} that stores a tree of Summary
+ * objects that matches the directory structure of the set of paths stored
+ * in the report.
+ */
 class TreeReport extends Report {
+  /**
+   * Map of detail nodes for this Report
+   *
+   * @hidden
+   */
   protected _details: { [index: string]: NodeOrLeaf };
 
   constructor(
@@ -18,6 +30,31 @@ class TreeReport extends Report {
     });
   }
 
+  /**
+   * Map of detail nodes for this report
+   *
+   * The nodes are keyed based on directory groups within the set of paths.
+   *
+   * @remarks
+   * The following tree structure would generate the following details map:
+   * ```
+   * Tree Structure:
+   *   |
+   *   |`- Leaf        (DetailedSummary)
+   *   |
+   *   `-- GroupA      (TreeReportNode)
+   *         |
+   *         |`- Leaf1 (DetailedSummary)
+   *         |
+   *         `-- Leaf2 (DetailedSummary)
+   *
+   * details:
+   * {
+   *   'Leaf': DetailedSummary,
+   *   'GroupA': TreeReportNode, // contains Leaf1 and Leaf2 as children
+   * }
+   *
+   */
   public get details(): { [index: string]: NodeOrLeaf } {
     return this._details;
   }
